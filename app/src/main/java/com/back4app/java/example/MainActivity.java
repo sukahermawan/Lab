a5d682686eddee6431235d28c662b5fd9cf477a1
Cihan Gül <cihangulx@gmail.com>
2020-12-04 14:48:11 +0300

    Parse SDK implementation

--- app/src/main/java/com/back4app/java/example/MainActivity.java
+++ app/src/main/java/com/back4app/java/example/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.parse.Parse;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.parse_app_id))
+                .clientKey(getString(R.string.parse_client_key))
+                .server(getString(R.string.parse_server_url))
+                .build());
     }
 }
