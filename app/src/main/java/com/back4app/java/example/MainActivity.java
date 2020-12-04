52e5902b4ad5327f64f5fcdaea911e0e1f0f8364
Cihan Gül <cihangulx@gmail.com>
2020-12-04 20:02:27 +0300

    Save example.

--- app/src/main/java/com/back4app/java/example/MainActivity.java
+++ app/src/main/java/com/back4app/java/example/MainActivity.java
@@ -3,8 +3,13 @@ package com.back4app.java.example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
                 .clientKey(getString(R.string.parse_client_key))
                 .server(getString(R.string.parse_server_url))
                 .build());
+
+        TextView textView = findViewById(R.id.textView);
+        ParseObject firstObject = new  ParseObject("FirstClass");
+        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+
+        firstObject.saveInBackground(e -> {
+            if (e != null){
+                Log.e("MainActivity", e.getLocalizedMessage());
+            }else{
+                Log.d("MainActivity","Object saved.");
+                textView.setText(String.format("Object saved. %s", firstObject.getObjectId()));
+            }
+        });
+
+
     }
 }
